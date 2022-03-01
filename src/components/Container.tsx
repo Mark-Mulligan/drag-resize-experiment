3b4bbffa0710f9f2173f6d1d4068749d6360bdcd
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-03-01 17:25:54 -0600

    inital app setup

---
+++ src/components/Container.tsx
@@ -0,0 +1,24 @@
+import styles from '../styles/Container.module.scss';
+
+interface ContainerProps {
+  children: JSX.Element;
+}
+
+const Container = ({ children }: ContainerProps) => {
+  return (
+    <div
+      style={{
+        height: 450,
+        width: 800,
+        margin: 'auto',
+        marginTop: '100px',
+        border: '2px solid black',
+        position: 'relative',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/components/ResizeableRect.tsx
@@ -0,0 +1,7 @@
+const ResizeableRect = () => {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, height: 100, width: 200, border: '2px solid green' }}></div>
+  );
+};
+
+export default ResizeableRect;

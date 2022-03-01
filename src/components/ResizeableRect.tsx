2c8cf2d4419d089fc15554d9de1ef68dc3089ec8
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-03-01 17:43:35 -0600

    added cursor animation

--- src/components/ResizeableRect.tsx
+++ src/components/ResizeableRect.tsx
@@ -1,6 +1,34 @@
+import { HtmlHTMLAttributes, MouseEvent, useState } from 'react';
+import styles from '../styles/ResizeableRect.module.scss';
+
 const ResizeableRect = () => {
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+    console.log(e);
+    setIsDragging(true);
+  };
+
+  const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
+    console.log(e);
+    setIsDragging(false);
+  };
+
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, height: 100, width: 200, border: '2px solid green' }}></div>
+    <div
+      className={isDragging ? 'dragging' : 'draggable'}
+      style={{
+        position: 'absolute',
+        top: position.top,
+        left: position.left,
+        height: 100,
+        width: 200,
+        border: '2px solid green',
+      }}
+      onMouseDown={(e) => handleMouseDown(e)}
+      onMouseUp={(e) => handleMouseUp(e)}
+    ></div>
   );
 };
 

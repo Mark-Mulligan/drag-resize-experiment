41126f6e719e3cced1fd6f193f239fffa83ded0c
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-03-01 18:20:34 -0600

    working on dragging functionality

--- src/components/ResizeableRect.tsx
+++ src/components/ResizeableRect.tsx
@@ -1,18 +1,37 @@
-import { HtmlHTMLAttributes, MouseEvent, useState } from 'react';
+import { useState, useCallback } from 'react';
 import styles from '../styles/ResizeableRect.module.scss';
 
 const ResizeableRect = () => {
   const [position, setPosition] = useState({ top: 0, left: 0 });
   const [isDragging, setIsDragging] = useState(false);
+  const [mouseStartingPos, setMouseStartingPos] = useState({ x: 0, y: 0 });
 
-  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
-    console.log(e);
+  const handleDrag = useCallback(
+    (e: MouseEvent) => {
+      let xDiff = e.screenX - mouseStartingPos.x;
+      let yDiff = e.screenY - mouseStartingPos.y;
+
+      console.log(mouseStartingPos.x);
+
+      const lastPosition = { ...position };
+      setPosition({ top: lastPosition.top + yDiff, left: lastPosition.left + xDiff });
+      //console.log(e.screenX, e.screenY);
+    },
+    [mouseStartingPos.x, mouseStartingPos.y],
+  );
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    setMouseStartingPos({ x: e.screenX, y: e.screenY });
     setIsDragging(true);
+
+    window.addEventListener('mousemove', handleDrag);
   };
 
-  const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
+  const handleMouseUp = (e: React.MouseEvent<HTMLDivElement>) => {
     console.log(e);
     setIsDragging(false);
+
+    window.removeEventListener('mousemove', handleDrag);
   };
 
   return (

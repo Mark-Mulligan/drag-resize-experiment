54b54d336cdf095a6b519eec6a115e8380be5555
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-03-02 22:35:12 -0600

    added resizing squares to rectangle

--- src/components/ResizeableRect.tsx
+++ src/components/ResizeableRect.tsx
@@ -1,7 +1,7 @@
 import { useState, useCallback, useEffect } from 'react';
 
 const ResizeableRect = () => {
-  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [position, setPosition] = useState({ top: 20, left: 20 });
   const [isDragging, setIsDragging] = useState(false);
   const [mouseStartingPos, setMouseStartingPos] = useState({ x: 0, y: 0 });
 
@@ -47,7 +47,52 @@ const ResizeableRect = () => {
       }}
       onMouseDown={(e) => handleMouseDown(e)}
       onMouseUp={(e) => handleMouseUp(e)}
-    ></div>
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: -6,
+          left: -6,
+          height: 12,
+          width: 12,
+          border: '1px solid black',
+          background: 'white',
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          top: -6,
+          right: -6,
+          height: 12,
+          width: 12,
+          border: '1px solid black',
+          background: 'white',
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          bottom: -6,
+          left: -6,
+          height: 12,
+          width: 12,
+          border: '1px solid black',
+          background: 'white',
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          bottom: -6,
+          right: -6,
+          height: 12,
+          width: 12,
+          border: '1px solid black',
+          background: 'white',
+        }}
+      />
+    </div>
   );
 };
 
